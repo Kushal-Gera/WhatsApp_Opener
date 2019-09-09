7dd1d6b508ff16feb45a131dcbe50c693db2a7f1
Kushal-Gera <kushalgera1212@gmail.com>
2019-09-09 21:59:41 +0530

    ui fixes

--- app/src/main/java/com/example/intentstuff/MainActivity.kt
+++ app/src/main/java/com/example/intentstuff/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.intentstuff
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
@@ -17,36 +19,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         val number = if (intent.action == Intent.ACTION_PROCESS_TEXT)
-                            intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
-
-                    else if ((intent.action == Intent.ACTION_DIAL) || (intent.action == Intent.ACTION_VIEW))
-                            intent?.data?.schemeSpecificPart.toString()
-
-                    else "8588910153"
+            intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
+        else if ((intent.action == Intent.ACTION_DIAL) || (intent.action == Intent.ACTION_VIEW))
+            intent?.data?.schemeSpecificPart.toString()
+        else
+            return
 
         startWhatsApp(number.trim())
 
     }
 
-    private fun startWhatsApp(number : String) {
+    private fun startWhatsApp(number: String) {
 
         val intent = Intent(Intent.ACTION_VIEW)
         intent.setPackage("com.whatsapp")
 
         var data = if (number[0] == '+')
-                        number.substring(1)
-                   else number
+            number.substring(1)
+        else number
 
         data = if (data.length == 10)
-                    "91$data"
-                else data
+            "91$data"
+        else data
 
         intent.data = Uri.parse("https://wa.me/$data")
 
-        if (packageManager.resolveActivity(intent,0) != null)
+        if (packageManager.resolveActivity(intent, 0) != null)
             startActivity(intent)
+        else
+            Toast.makeText(this, "Please Install WhatsApp!!", Toast.LENGTH_SHORT).show()
 
-        else Toast.makeText(this, "Please Install WhatsApp!!", Toast.LENGTH_SHORT).show()
         finish()
 
     }
