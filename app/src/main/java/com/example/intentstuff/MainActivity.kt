a8ce89506c20c8ef007b04279b59b4ceb25c642b
Kushal-Gera <kushalgera1212@gmail.com>
2019-09-12 21:12:49 +0530

    'Hide' Bug Rised and Not Fixed

--- app/src/main/java/com/example/intentstuff/MainActivity.kt
+++ app/src/main/java/com/example/intentstuff/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.intentstuff
 
-import android.content.ComponentName
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,30 +13,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hide.setOnClickListener {
-            val p = packageManager
-            val componentName = ComponentName(
-                this,
-                MainActivity::class.java
-            )
-            p.setComponentEnabledSetting(
-                componentName,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-            )
-            Toast.makeText(this, "Hidden", Toast.LENGTH_SHORT).show()
-            finish()
-        }
+        //place it before intent and get stuff....
+        hide_icon()
 
         val number = if (intent.action == Intent.ACTION_PROCESS_TEXT)
-                intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
-            else if ((intent.action == Intent.ACTION_DIAL) || (intent.action == Intent.ACTION_VIEW))
-                intent?.data?.schemeSpecificPart.toString()
-            else
-              return
+            intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
+        else if ((intent.action == Intent.ACTION_DIAL) || (intent.action == Intent.ACTION_VIEW))
+            intent?.data?.schemeSpecificPart.toString()
+        else
+            return
 
         startWhatsApp(number.trim())
 
+
+    }
+
+    private fun hide_icon() {
+        hide.setOnClickListener {
+            Toast.makeText(this, "In Working Phase", Toast.LENGTH_SHORT).show()
+        }
+
+//        hide.setOnClickListener {
+//            val p = packageManager
+//            val componentName = ComponentName(
+//                this,
+//                MainActivity::class.java
+//            )
+//            p.setComponentEnabledSetting(
+//                componentName,
+//                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+//                PackageManager.DONT_KILL_APP
+//            )
+//            Toast.makeText(this, "Hidden", Toast.LENGTH_SHORT).show()
+//            finish()
+//        }
 
     }
 
